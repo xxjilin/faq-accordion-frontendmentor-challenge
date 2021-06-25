b7ba039b6042cfb0e4a1d978ad3c06c32bed3d85
Angelyn Dequito <angedequito02@gmail.com>
2021-06-26 01:07:26 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// add selector 
+
+// var acc = document.getElementsByClassName("question");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     /* Toggle between adding and removing the "active" class,
+//     to highlight the button that controls the panel */
+//     this.classList.toggle("active");
+
+//     /* Toggle between hiding and showing the active panel */
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+
+// const questions = document.getElementsByClassName('question');
+
+// questions.forEach(question => {
+//   question.addEventListener('click', (e) => {
+//     questions.forEach(question => {
+//       if(question.active){
+//         question.active = false;
+//         question.active = true;
+//       }
+//     })
+//   })
+// })
+2
+
+var acc = document.getElementsByClassName("question_answer__accordion");
+var panel = document.getElementsByClassName('question');
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        var setClasses = !this.classList.contains('active');
+        setClass(acc, 'active', 'remove');
+        setClass(panel, 'show', 'remove');
+
+        if (setClasses) {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
+}
+
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+    }
+}
